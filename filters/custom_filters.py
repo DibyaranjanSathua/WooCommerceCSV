700b18c1b4e0f40afdebfb61978e2febd71401df
dibyaranjan sathua <dibyaranjan.sathua@gmail.com>
2021-01-01 12:51:28 +0530

    mapping file modified

--- filters/custom_filters.py
+++ filters/custom_filters.py
@@ -3,6 +3,7 @@ File:           custom_filters.py
 Author:         Dibyaranjan Sathua
 Created on:     29/12/2020, 15:17
 """
+from typing import Union
 import re
 
 
@@ -10,3 +11,10 @@ def remove_leading_asterisk(string: str):
     """ Removing leading asterisk characters """
     regex = re.compile(r"\s*\*\s*")
     return regex.sub("", string)
+
+
+def blank_if_zero(value: Union[float, int]):
+    """ Remove zero from the entry. If the value is zero, make it blank """
+    if value == 0:
+        return ""
+    return value
