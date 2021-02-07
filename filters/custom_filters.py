1b4437afead99a68da51807a7c8fbf44c824b4d3
dibyaranjansathua <dibyaranjan.sathua@gmail.com>
2021-02-07 20:43:46 +0530

    Bromic mapping file

--- filters/custom_filters.py
+++ filters/custom_filters.py
@@ -6,26 +6,108 @@ Created on:     29/12/2020, 15:17
 from typing import Union
 import re
 import os
+import ftfy
 
 
 def remove_leading_asterisk(string: str):
-    """ Removing leading asterisk characters """
+    """ Removing leading asterisk characters. Used in brema """
     regex = re.compile(r"\s*\*\s*")
     return regex.sub("", string)
 
 
 def blank_if_zero(value: Union[float, int]):
-    """ Remove zero from the entry. If the value is zero, make it blank """
+    """ Remove zero from the entry. If the value is zero, make it blank. Used in brema """
     if value == 0:
         return ""
     return value
 
 
 def file_exist(filename: str, base_path: str):
-    """ Check if file exist on the server """
+    """ Check if file exist on the server. Used in brema """
     if not filename:
         return False
     filepath = os.path.join(os.path.expanduser("~"), base_path, filename)
     if os.path.isfile(filepath):
         return True
     return False
+
+
+def remove_trailing_bromic(string: str):
+    """ Remove bromic from the end of the string. Used in bromic """
+    regex = re.compile(r"\s*-\s*Bromic\s*$")
+    return regex.sub("", string, re.IGNORECASE)
+
+
+def add_leading_bromic(string: str):
+    """ Add leading bromic if not present. Used in bromic """
+    regex = re.compile(r"^\s*Bromic")
+    if regex.search(string) is None:
+        return f"Bromic {string}"
+    return string
+
+
+def remove_div_span_tags(string: str):
+    """ Remove div and span tags. Used in bromic """
+    div_start_regex = re.compile(r"<div.*?>")
+    div_end_regex = re.compile(r"</div>")
+    span_start_regex = re.compile(r"<span.*?>")
+    span_end_regex = re.compile(r"</span>")
+    output_string = div_start_regex.sub("", string)
+    output_string = div_end_regex.sub("", output_string)
+    output_string = span_start_regex.sub("", output_string)
+    output_string = span_end_regex.sub("", output_string)
+    return output_string
+
+
+def non_zero_warranty(string: str):
+    """ Return 1 if the warrant value if zero """
+    return "1" if string == "0" else string
+
+
+def product_files(string: str, path_prefix: str):
+    """ Extract product files from additional attributes. Used in bromic """
+    regex = re.compile(r"products_file_text=(.*?),")
+    match_str = regex.search(string)
+    if match_str is None:
+        return ""
+    product_file_text = match_str.group(1)
+    files = product_file_text.split("|")
+    output = ""
+    for file in files:
+        if not file:
+            continue
+        name, path = file.split("&")
+        # Remove Download from the name
+        name = name.replace("Download", "").strip()
+        output += f'<a href="{path_prefix}{path}" target="_blank">{name}</a> | '
+
+    output = output.strip().strip("|").strip()
+    return output
+
+
+def create_image(string: str, path_prefix: str):
+    """ Image path. Used in bromic """
+    images = [f"{path_prefix}{x.strip()}" for x in string.split(",")]
+    return ",".join(images)
+
+
+def format_description(string: str):
+    """ Format description. Used in bromic """
+    def remove_leading_line_space(match_str):
+        return ""
+    regex = re.compile(r"^\s+", re.MULTILINE)
+    return regex.sub(remove_leading_line_space, string)
+
+
+def repair_utf8_encoding(string: str):
+    """ Repair utf-8 encoding containing iso-8599 """
+    return ftfy.fix_encoding(string)
+
+
+def format_volume(string: str):
+    """ Format volume. Used in bromic """
+    regex = re.compile(r"(\d)+")
+    match_str = regex.search(string)
+    if match_str is None:
+        return 0
+    return float(match_str.group(1))
--- src/supplier_csv_to_woocommerce_csv.py
+++ src/supplier_csv_to_woocommerce_csv.py
@@ -22,16 +22,19 @@ class SupplierCSV2WoocommerceCSV:
 
     def process_supplier_csv(self):
         """ Process supplier csv file and use it to render the template """
+        # Previously for brema, we have used "cp1252"
+        # Input encoding "cp437" is working fine for both brema, bromic
         self._supplier_df = pd.read_csv(
             self._csv_file,
             header=0,
-            encoding="cp1252",
+            encoding="cp437",
             na_filter=False
         )
         self._supplier_records = self._supplier_df.to_dict("records")
 
     def convert(self):
         """ Convert supplier CSV to WooCommerce CSV"""
+        counter = 0
         self.process_supplier_csv()
         for record in self._supplier_records:
             output = Template.render(template=self._template, record=record)
@@ -42,11 +45,13 @@ class SupplierCSV2WoocommerceCSV:
             except Exception as err:
                 print(err)
                 print(output)
+                counter += 1
+        print(f"No of products with wrong syntax: {counter}")
 
     def save_to_csv(self, output_csv):
         """ Save the woocommerce data to csv """
         self._woocommerce_df = pd.DataFrame(self._woocommerce_records)
-        self._woocommerce_df.to_csv(output_csv, encoding="cp1252", index=False)
+        self._woocommerce_df.to_csv(output_csv, index=False)
 
     @property
     def product_records(self) -> List[Dict]:
@@ -55,8 +60,16 @@ class SupplierCSV2WoocommerceCSV:
 
 
 if __name__ == "__main__":
+    # obj = SupplierCSV2WoocommerceCSV(
+    #     csv_file="/Users/dibyaranjan/Upwork/client_nick_woocommerce/brema.csv",
+    #     template="brema.yml",
+    # )
+    # obj.convert()
+    # obj.save_to_csv("/tmp/test_brema.csv")
+
     obj = SupplierCSV2WoocommerceCSV(
-        csv_file="/Users/dibyaranjan/Upwork/client_nick_woocommerce/brema.csv",
-        template="brema.yml",
+        csv_file="/Users/dibyaranjan/Upwork/client_nick_woocommerce/bromic.csv",
+        template="bromic.yml"
     )
     obj.convert()
+    obj.save_to_csv("/tmp/test_bromic.csv")
